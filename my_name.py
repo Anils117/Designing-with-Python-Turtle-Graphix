de28d89cf60f20049823d55715c761447f985f4b
Anil Mallem <83568202+Anils117@users.noreply.github.com>
2021-06-01 15:54:56 +0530

    Create my_name.py

---
+++ my_name.py
@@ -0,0 +1,184 @@
+import turtle
+ak=turtle.Turtle()
+ak.color("white")
+ak.speed(0)
+turtle.bgcolor("black")
+i=1
+###############################################AAAAAAAAAAAAAAAAAAAAA
+while i<15:
+	ak.fd(50)
+	ak.up()
+	ak.fd(50)
+	ak.down()
+	ak.fd(50)
+	ak.up()
+	ak.left(90)
+	ak.fd(1)
+	ak.left(90)
+	ak.down()
+	ak.fd(50)
+	ak.up()
+	ak.fd(50)
+	ak.down()
+	ak.fd(50)
+	ak.up()
+	ak.right(90)
+	ak.fd(1)
+	ak.right(90)
+	ak.down()
+	i=i+1
+while i<=25:
+	ak.fd(150)
+	ak.up()
+	ak.left(90)
+	ak.fd(1)
+	ak.left(90)
+	ak.down()
+	ak.fd(150)
+	ak.up()
+	ak.right(90)
+	ak.fd(1)
+	ak.right(90)
+	ak.down()
+	i=i+1
+while i<=40:
+	ak.fd(50)
+	ak.up()
+	ak.fd(50)
+	ak.down()
+	ak.fd(50)
+	ak.up()
+	ak.left(90)
+	ak.fd(1)
+	ak.left(90)
+	ak.down()
+	ak.fd(50)
+	ak.up()
+	ak.fd(50)
+	ak.down()
+	ak.fd(50)
+	ak.up()
+	ak.right(90)
+	ak.fd(1)
+	ak.right(90)
+	ak.down()
+	i=i+1
+
+while i<=50:
+	ak.fd(150)
+	ak.up()
+	ak.left(90)
+	ak.fd(1)
+	ak.left(90)
+	ak.down()
+	ak.fd(150)
+	ak.up()
+	ak.right(90)
+	ak.fd(1)
+	ak.right(90)
+	ak.down()
+	i=i+1
+
+
+#############################################################NNNNNNNNNNNNNNNNNNNNNNNNN
+ak.up()
+ak.fd(160)
+ak.down()
+
+i=0
+a=56
+while i<=60:
+	ak.fd(25)
+	ak.up()
+	ak.fd(50-a)
+	ak.down()
+	ak.fd(25)
+	ak.up()
+	ak.fd(a)
+	ak.down()
+	ak.fd(25)
+	ak.up()
+	ak.right(180)
+	ak.fd(125)
+	ak.left(90)
+	ak.fd(1)
+	ak.left(90)
+	ak.down()
+
+
+	a=a-1
+	i=i+1
+
+#########################################################IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII
+ak.up()
+ak.fd(135)
+ak.down()
+
+i=0
+while i<=15:
+	ak.fd(100)
+	ak.up()
+	ak.right(180)
+	ak.fd(100)
+	ak.right(90)
+	ak.fd(1)
+	ak.right(90)
+	ak.down()
+	i=i+1
+while i<=47:
+	ak.up()
+	ak.fd(35)
+	ak.down()
+	ak.fd(30)
+	ak.up()
+	ak.fd(30)
+	ak.right(180)
+	ak.fd(95)
+	ak.right(90)
+	ak.fd(1)
+	ak.right(90)
+	ak.down()
+	i=i+1
+while i<=62:
+	ak.fd(100)
+	ak.up()
+	ak.right(180)
+	ak.fd(100)
+	ak.right(90)
+	ak.fd(1)
+	ak.right(90)
+	ak.down()
+	i=i+1
+
+
+###########################################################LLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLL
+ak.up()
+ak.fd(110)
+ak.down()
+
+i=0
+while i<50:
+	ak.fd(35)
+	ak.up()
+	ak.fd(65)
+	ak.right(180)
+	ak.fd(100)
+	ak.left(90)
+	ak.fd(1)
+	ak.left(90)
+	ak.down()
+	i=i+1
+
+while i<65:
+	ak.fd(100)
+	ak.up()
+	ak.right(180)
+	ak.fd(100)
+	ak.left(90)
+	ak.fd(1)
+	ak.left(90)
+	ak.down()
+	i=i+1
+
+turtle.exitonclick()
+	
